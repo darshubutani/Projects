95368cc5b8d5d141165e58313034cb34c0e1bce3
darshubutani <116871718+darshubutani@users.noreply.github.com>
2022-11-10 10:00:23 +0530

    Modal project done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+console.log(btnShowModal);
 
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const showModal = function () {
+    console.log("clicked");
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+document.addEventListener('keydown',function(e){
+if (e.key==='Escape' && !modal.classList.contains('.hidden'))
+closeModal();
+});
+
+for (let i = 0; i < btnShowModal.length; i++)
+    btnShowModal[i].addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
